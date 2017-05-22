5a94f1db50dc29e17de63bf9594acc52de15fc0b
Andrzej Dybionka <andrzej@arabel.la>
2017-05-22 06:57:09 +0200

    - map immutable map to js

--- dist/index.js
+++ dist/index.js
@@ -6,6 +6,9 @@ Object.defineProperty(exports, "__esModule", {
 
 var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
 
+var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
+
+exports.toJS = toJS;
 exports.setHeaders = setHeaders;
 exports.setBaseUrl = setBaseUrl;
 
@@ -25,9 +28,16 @@ var _axios$defaults = _axios2.default.defaults,
 
 
 var apiClient = _axios2.default.create({
-  transformRequest: [_humps2.default.decamelizeKeys].concat(transformRequest),
+  transformRequest: [toJS, _humps2.default.decamelizeKeys].concat(transformRequest),
   transformResponse: transformResponse.concat(_humps2.default.camelizeKeys)
 });
+
+function toJS(map) {
+  if ((typeof map === 'undefined' ? 'undefined' : _typeof(map)) === 'object' && typeof map.toJS === 'function') {
+    return map.toJS();
+  }
+  return map;
+}
 
 function setHeaders(headers) {
   apiClient.defaults.headers = _extends({}, apiClient.defaults.headers, headers);
